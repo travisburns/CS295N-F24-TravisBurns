c3283ca55bb5eccec1456b53e1121afab4fffa04
travisburns <travisjb0@gmail.com>
2024-10-24 20:36:45 -0700

    FInished Lab03, Added Bootstrap classes to the post, as I just didnt like the overall feel of the post.

--- AnimeInfo/AnimeInfo/Controllers/BlogController.cs
+++ AnimeInfo/AnimeInfo/Controllers/BlogController.cs
@@ -2,16 +2,24 @@
 using Microsoft.AspNetCore.Mvc;
 
 
+
 namespace AnimeInfo.Controllers
 {
     public class BlogController : Controller
     {
+
+        private static List<Blog> blogPosts = new List<Blog>();
+
         public IActionResult Index()
         {
-            //TODO: get the blog objects and put them into a list. 
-            List<Blog> blogs;
+            return View(blogPosts);
+        }
 
-            return View();
+        public IActionResult Post()
+        {
+            Blog model = new Blog();
+            model.BlogAuthor = new AppUser();
+            return View(model);
         }
 
         public IActionResult About()
@@ -27,6 +35,19 @@ namespace AnimeInfo.Controllers
         public IActionResult Blogs()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Blog model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.BlogDate = DateTime.Now;
+
+                blogPosts.Add(model);
+                return RedirectToAction("Index");  
+            }
+            return View(model);
         }
     }
 }
--- AnimeInfo/AnimeInfo/Models/AppUser.cs
+++ AnimeInfo/AnimeInfo/Models/AppUser.cs
@@ -2,7 +2,7 @@
 {
     public class AppUser
     {
-        public string UserName { get; set; }
+        public string Name { get; set; }
 
         public DateTime SignUpdate { get; set; }
 
