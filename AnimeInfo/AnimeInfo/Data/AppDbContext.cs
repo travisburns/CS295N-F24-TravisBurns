f0271f3d52d30f8b0b3901f5f4464025cac5313a
travisburns <travisjb0@gmail.com>
2024-11-21 22:16:22 -0800

    Lab6 Completed

---
+++ AnimeInfo/AnimeInfo/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AnimeInfo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeInfo.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+
+        // constructor just calls the base class constructor
+
+        public ApplicationDbContext(
+
+           DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+
+        // one DbSet for each domain model class
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
+
+
--- AnimeInfo/AnimeInfo/Models/AppUser.cs
+++ AnimeInfo/AnimeInfo/Models/AppUser.cs
@@ -2,9 +2,8 @@
 {
     public class AppUser
     {
+        public int Id { get; set; }  // This will be your primary key
         public string Name { get; set; }
-
         public DateTime SignUpdate { get; set; }
-
     }
 }
