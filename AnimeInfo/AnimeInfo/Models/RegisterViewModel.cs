7a1743ea65f1916a60ac1d7b17a9cbec3a423169
travisburns <travisjb0@gmail.com>
2025-01-11 15:49:23 -0800

    Added Registration Page and MVC for it

---
+++ AnimeInfo/AnimeInfo/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimeInfo.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
